24c348aad8d63727e1391df13fade2cbc9d89541
Tomasz Matras <tmatras@whitelabelcoders.com>
2024-01-11 22:34:50 +0100

    Complete validation and add alerts

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -15,7 +15,15 @@ export class RegisterComponent {
     Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm),
   ]);
   confirm_password = new FormControl('', [Validators.required]);
-  phoneNumber = new FormControl('');
+  phoneNumber = new FormControl('', [
+    Validators.required,
+    Validators.minLength(15),
+    Validators.maxLength(15),
+  ]);
+
+  showAlert: boolean = false;
+  alertMsg: string = 'Please wait! Your account is being created';
+  alertColor: string = 'blue';
 
   registerForm = new FormGroup({
     name: this.name,
@@ -25,4 +33,10 @@ export class RegisterComponent {
     confirm_password: this.confirm_password,
     phoneNumber: this.phoneNumber,
   });
+
+  register() {
+    this.showAlert = true;
+    this.alertMsg = 'Please wait! Your account is being created';
+    this.alertColor = 'blue';
+  }
 }
