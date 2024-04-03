00587c79292e7a38dde04bb0a3d9e19a3db8a980
Tomasz Matras <tmatras@whitelabelcoders.com>
2024-04-03 20:00:30 +0200

    Add query-params to store and define sort option i manage component

--- src/app/video/menage/menage.component.ts
+++ src/app/video/menage/menage.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-menage',
@@ -6,7 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menage.component.css'],
 })
 export class MenageComponent implements OnInit {
-  constructor() {}
+  videoOrder: string = '1';
 
-  ngOnInit(): void {}
+  constructor(private router: Router, private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.route.queryParams.subscribe((params: Params) => {
+      this.videoOrder = params['sort'] === '2' ? params['sort'] : 1;
+    });
+  }
+
+  sort(event: Event) {
+    const { value } = event.target as HTMLSelectElement;
+
+    // this.router.navigateByUrl(`/manage?sort=${value}`);
+
+    // inna metoda dodawania query params
+
+    this.router.navigate([], {
+      relativeTo: this.route,
+      queryParams: {
+        sort: value,
+      },
+    });
+  }
 }
