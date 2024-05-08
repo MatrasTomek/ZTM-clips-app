e053eeb5ba54c5c5fcd1bb1afcef070123053c14
Tomasz Matras <tmatras@whitelabelcoders.com>
2024-05-08 20:43:07 +0200

    Add update fn to clips service

--- src/app/video/edit/edit.component.ts
+++ src/app/video/edit/edit.component.ts
@@ -1,7 +1,16 @@
-import { Component, OnInit, OnDestroy, Input, OnChanges } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  OnDestroy,
+  Input,
+  OnChanges,
+  Output,
+  EventEmitter,
+} from '@angular/core';
 import IClip from 'src/app/models/clip.model';
 import { ModalService } from 'src/app/services/modal.service';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ClipService } from 'src/app/services/clip.service';
 
 @Component({
   selector: 'app-edit',
@@ -10,6 +19,12 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class EditComponent implements OnInit, OnDestroy, OnChanges {
   @Input() activeClip: IClip | null = null;
+  inSubmission = false;
+  showAlert = false;
+  alertColor = 'blue';
+  alertMsg = 'Please wait! Update in progress.';
+
+  @Output() update = new EventEmitter();
 
   clipID = new FormControl('');
   title = new FormControl('', [Validators.required, Validators.minLength(3)]);
@@ -18,7 +33,7 @@ export class EditComponent implements OnInit, OnDestroy, OnChanges {
     id: this.clipID,
   });
 
-  constructor(private modal: ModalService) {}
+  constructor(private modal: ModalService, private clipService: ClipService) {}
 
   ngOnInit(): void {
     this.modal.register('editClip');
@@ -27,11 +42,42 @@ export class EditComponent implements OnInit, OnDestroy, OnChanges {
     if (!this.activeClip) {
       return;
     }
-
+    this.inSubmission = false;
+    this.showAlert = false;
     this.clipID.setValue(this.activeClip.docID as string);
     this.title.setValue(this.activeClip.title);
   }
   ngOnDestroy() {
     this.modal.unregister('editClip');
+  }
+
+  async submit() {
+    if (!this.activeClip) {
+      return;
+    }
+
+    this.inSubmission = true;
+    this.showAlert = true;
+    this.alertColor = 'blue';
+    this.alertMsg = 'Please wait! Update in progress.';
+
+    try {
+      await this.clipService.updateClip(
+        this.clipID.value as string,
+        this.title.value as string
+      );
+    } catch (err) {
+      this.inSubmission = false;
+      this.alertColor = 'red';
+      this.alertMsg = 'Something wrong, try later.';
+      return;
+    }
+
+    this.activeClip.title = this.title.value as string;
+    this.update.emit(this.activeClip);
+
+    this.inSubmission = false;
+    this.alertColor = 'green';
+    this.alertMsg = 'Success! Clip updated.';
   }
 }
--- src/app/video/menage/menage.component.ts
+++ src/app/video/menage/menage.component.ts
@@ -59,4 +59,12 @@ export class MenageComponent implements OnInit {
     this.activeClip = clip;
     this.modal.toggleModal('editClip');
   }
+
+  update($event: IClip) {
+    this.clips.forEach((element, index) => {
+      if (element.docID == $event.docID) {
+        this.clips[index].title = $event.title;
+      }
+    });
+  }
 }
